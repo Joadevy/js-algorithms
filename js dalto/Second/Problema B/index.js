107e88cbfcf70439224e42a619bd466372793192
Joaquin Arlettaz <jjoaquinarlettaz@gmail.com>
2022-01-12 10:59:29 -0300

    Problem B documentated

--- js dalto/Second/Problema B/index.js
+++ js dalto/Second/Problema B/index.js
@@ -1,15 +1,18 @@
+/* This is a script for register the class attendance. The script uses a matrix and firstly prompts for the amount of students,
+then asks for the today-status (this process should be repeated for a month), then saves it in the matrix with the respective name. 
+Finally alerts for the students who have more than 2 non-attendance because they are dissaproved. */
+
 var students = [];
 var amountStudents = parseInt(prompt('Enter the amount of students: ',''));
 
-
-const loadStudent = (array,amount) => {
+const loadStudent = (array,amount) => { // Loads the name and initialize the non-attendance in 0 for each student.
     for (let i=0;i<amount;i++) {
        array[i] = [prompt("Enter the name: "),0];
     }
 }
 
-const loadAttendance = (array,amount) => {
-    for (let x=0;x<2;x++) {
+const loadAttendance = (array,amount) => { // loads the class-attendance for each student.
+    for (let x=0;x<5;x++) { // It consideres only 5 days of attendance registering.
         for (let i=0;i<amount;i++) {
             let yesNo = prompt(`Is ${array[i][0]} on class? (y/n): `,'');
                 if ((yesNo == 'n') || (yesNo == 'no')) {
@@ -19,9 +22,9 @@ const loadAttendance = (array,amount) => {
     }
 }
 
-const finalStatus = (array,amount) => {
+const finalStatus = (array,amount) => { // Shows the final-status of each student. If they have more than 2 non-attendance, they are dissaproved.
     for (let i=0;i<amount;i++) {
-        if (array[i][1] > 1) {
+        if (array[i][1] > 2) {
             alert (`The student ${array[i][0]} is dissaproved`);
         }
     }
@@ -30,5 +33,4 @@ const finalStatus = (array,amount) => {
 loadStudent(students,amountStudents);
 loadAttendance(students,amountStudents);
 finalStatus(students,amountStudents);
-document.write(students);
 
