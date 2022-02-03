7993f29b06d0fa489e4d46fe4781bfe772cd8ede
Joaquin Arlettaz <jjoaquinarlettaz@gmail.com>
2022-02-03 13:15:46 -0300

    web styled

--- js dalto course/6 - Chapter/Problem/code.js
+++ js dalto course/6 - Chapter/Problem/code.js
@@ -1,7 +1,7 @@
 const container = document.querySelector(".container");
 
 function generateKey(name,model,price){ // Returns an array with the data of the key.
-    img = "<img src='genericKey.png'>";
+    img = "<img src='genericKey.png' class='key-img'>";
     name = `<h2>Key ${name}</h2>`;
     model = `<h3>Model ${model}</h3>`;
     price = `<p>Price: <b>${price}</b></p>`;
@@ -10,12 +10,13 @@ function generateKey(name,model,price){ // Returns an array with the data of the
 
 let documentFragment = document.createDocumentFragment();
 
-for(let i=0; i<20;i++) {
+for(let i=1; i<=20;i++) {
     let div = document.createElement("DIV");
     div.classList.add(`item-${i}`,`flex-item`);
     let modelRandom = Math.round(Math.random()*1000);
     let priceRandom = Math.round(Math.random()*10+30);
-    const key = generateKey(i+1,modelRandom,priceRandom);
+    const key = generateKey(i,modelRandom,priceRandom);
+    div.tabIndex = i;
     div.innerHTML = key[0] + key[1] + key[2] + key[3];
     documentFragment.appendChild(div);
 }
