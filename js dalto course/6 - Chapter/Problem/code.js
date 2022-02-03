708c19eecac5bbca402f5f5e90a4ec14bd064f35
Joaquin Arlettaz <jjoaquinarlettaz@gmail.com>
2022-02-03 12:39:26 -0300

    Updated code

--- js dalto course/6 - Chapter/Problem/code.js
+++ js dalto course/6 - Chapter/Problem/code.js
@@ -3,15 +3,22 @@ const container = document.querySelector(".container");
 
 function generateKey(name,model,price){ // Returns an array with the data of the key.
     img = "<img src='genericKey.png'>";
-    name = `<h2>${name}</h2>`;
-    model = `<h3>${model}</h3>`;
+    name = `<h2>Key ${name}</h2>`;
+    model = `<h3>Model ${model}</h3>`;
     price = `<p>Price: <b>${price}</b></p>`;
         return [img,name,model,price];
 }
 
+let documentFragment =  ;
+
 for(let i=0; i<20;i++) {
-    let random = Math.random()*10+30;
-    random = Math.round(random);
-    const key = generateKey(`Key ${i+1}`, `Model y${i+5}`,`${random}`);
-    container.innerHTML += key[0] + key[1] + key[2] + key[3];
+    let div = document.createElement("DIV");
+    div.classList.add(`item-${i}`,`flex-item`);
+    let modelRandom = Math.random()*1000;
+        modelRandom = Math.round(modelRandom);
+    let priceRandom = Math.random()*10+30;
+        priceRandom = Math.round(priceRandom);
+    const key = generateKey(i+1,modelRandom,priceRandom);
+    div.innerHTML = key[0] + key[1] + key[2] + key[3];
+
 }
