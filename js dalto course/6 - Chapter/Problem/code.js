fbb6dd5a7a8352283829b56dc926047f1208f428
Joaquin Arlettaz <jjoaquinarlettaz@gmail.com>
2022-02-03 16:43:01 -0300

    Documentated

--- js dalto course/6 - Chapter/Problem/code.js
+++ js dalto course/6 - Chapter/Problem/code.js
@@ -1,6 +1,6 @@
-const container = document.querySelector(".container");
+const container = document.querySelector(".container"); // Select the HTML div element with the class .container
 
-function generateKey(name,model,price){ // Returns an array with the data of the key.
+function generateKey(name,model,price){ // Returns an array with the HTML data of the key.
     img = "<img src='src/genericKey.png' class='key-img'>";
     name = `<h2>Key ${name}</h2>`;
     model = `<h3>Model ${model}</h3>`;
@@ -8,17 +8,22 @@ function generateKey(name,model,price){ // Returns an array with the data of the
         return [img,name,model,price];
 }
 
-let documentFragment = document.createDocumentFragment();
+let documentFragment = document.createDocumentFragment(); // Creates the fragments for load all the keys.
 
-for(let i=1; i<=20;i++) {
-    let div = document.createElement("DIV");
-    div.classList.add(`item-${i}`,`flex-item`);
-    let modelRandom = Math.round(Math.random()*1000);
-    let priceRandom = Math.round(Math.random()*10+30);
-    const key = generateKey(i,modelRandom,priceRandom);
-    div.tabIndex = i;
-    div.innerHTML = key[0] + key[1] + key[2] + key[3];
-    documentFragment.appendChild(div);
+const changeHidden = (number) => {
+    
 }
 
-container.appendChild(documentFragment);
+for(let i=1; i<=20;i++) { // Prepares and loads all of the 20 keys with data for each one.
+    let div = document.createElement("DIV"); // create a new element div.
+    div.classList.add(`item-${i}`,`flex-item`); // Add two classes
+    let modelRandom = Math.round(Math.random()*1000); // Generates a random number between 0-1000 and rounds it.
+    let priceRandom = Math.round(Math.random()*10+30); // Generates a random number between 30-40 and rounds it.
+    const key = generateKey(i,modelRandom,priceRandom);
+    div.addEventListener("click", () => changeHidden(modelRandom));
+    div.tabIndex = i; // Put tabindex
+    div.innerHTML = key[0] + key[1] + key[2] + key[3]; // Uses this notation because key it's an array and only uses the first 3 cells.
+    documentFragment.appendChild(div); // adds each child-div into the fragment.
+}
+
+container.appendChild(documentFragment); // Load the child-fragment into the container.
