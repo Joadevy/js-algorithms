2a7707bf6ffda10f796dd660daa7fa55f5ca5fe0
Joaquin Arlettaz <jjoaquinarlettaz@gmail.com>
2022-02-06 11:19:21 -0300

    Added an option to open amazon

--- js dalto course/Mid level/7 - Chapter/Problem A/problemA.js
+++ js dalto course/Mid level/7 - Chapter/Problem A/problemA.js
@@ -6,9 +6,12 @@ const purchaseAlert = (trueFalse) => { // Compares the client decision, if it is
         const orderNumber = Math.round(Math.random()*1000+30);
         document.write('Order proccessing. Your order-number is ' + orderNumber);
     } else {
-        alert ('We hope you can find a better one.');
-        document.write('You can find more PCs here: ');
-        window.open('https://www.amazon.com/s?k=laptop&i=computers-intl-ship&__mk_es_US=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=2LC8MNC94MFSK&sprefix=la%2Ccomputers-intl-ship%2C278&ref=nb_sb_noss_2');
+        let decision = window.confirm('We hope you can find a better one, want to take a look at? ');
+        if (decision == true) {
+            window.open('https://www.amazon.com/s?k=laptop&i=computers-intl-ship&__mk_es_US=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=2LC8MNC94MFSK&sprefix=la%2Ccomputers-intl-ship%2C278&ref=nb_sb_noss_2');
+        } else {
+            document.write('Thanks for visiting us. Have a good day!');
+        }
     }
 } 
 
