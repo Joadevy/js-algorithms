0d7a6f9b773efd34d9b3e50b52b1fe89e11dfad2
Joaquin Arlettaz <jjoaquinarlettaz@gmail.com>
2022-02-08 18:03:54 -0300

    Problem B working clean!

--- js dalto course/Mid level/9 - Chapter/problemB/index.js
+++ js dalto course/Mid level/9 - Chapter/problemB/index.js
@@ -1,6 +1,39 @@
 // Selecting the button
 const button = document.querySelector(".confirm-button");
 
+// Selecting the error-container
+const errorMessageDiv = document.getElementById("error-container");
+
+// Adding the listener for click to execute changes in the week for the exam.
+button.addEventListener("click", (e)=> {
+    e.preventDefault();
+    let valid = validate();
+    if (valid != true) {
+        errorMessageDiv.classList.remove('showError');
+        let elements = document.querySelectorAll(".week");
+        let selectedWeeks = document.querySelectorAll(".selected-week");
+        for (let element in elements) {
+            let week = elements[element];
+            week.innerHTML = selectedWeeks[element].value;
+            errorMessageDiv.textContent = "You need to select all the weeks!";
+        }
+    } else {
+        errorMessageDiv.classList.add('showError');
+        errorMessageDiv.textContent = "You need to select all the weeks!";
+    }
+})
+
+const validate = () => {
+    let weeks = document.querySelectorAll(".selected-week");
+    for (let week in weeks) {
+        if (weeks[week].value == "") {
+            return true
+        }
+    }
+    return false;
+}
+
+
 // Creating an array with some objects.
 let students = [{
     name: "Hector Herrera",
@@ -31,8 +64,9 @@ for (let student in students) {
    <div class="grid-item subject">${subject}</div>
    <div class="grid-item week">
         <select class="selected-week" name="" id="">
-            <option value="1">First week</option>
-            <option value="2">Second week</option>
+            <option value="" disabled selected>Select week</option>
+            <option value="1st week">First week</option>
+            <option value="2nd week">Second week</option>
         </select>
     </div>`
     document.querySelector('.grid-container').innerHTML += htmlCode;
