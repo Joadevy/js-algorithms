342f3a34514e98099ab24f2d1feed51a7231ea53
Joaquin Arlettaz <jjoaquinarlettaz@gmail.com>
2022-02-10 11:10:12 -0300

    Problem A documentated

--- js dalto course/Mid level/10 - Chapter/index.js
+++ js dalto course/Mid level/10 - Chapter/index.js
@@ -1,32 +1,34 @@
+// Getting the HTML elements.
 const studentName = document.querySelector('.student');
 const submitButton = document.getElementById("submit");
 const score = document.getElementById("score");
 const result = document.querySelector('.result');
+
+// Tracks the students object for loading the score-test and getting a result one by one.
 let student = 0;
 
-
+// Adding the listener for the button.
 submitButton.addEventListener('click', (e)=> {
         e.preventDefault();
         let valid = validateScore();
         if (valid == true) {
-             // *********** remove it then 
-            result.textContent = '';
+            result.textContent = ''; // Reset if there was an error message by a wrong char introduced instead a number.
             let averageScores = calculateAverage(student);
             if (averageScores >= 7) {
                 alert(`${students[student].name} approves!`);
             } else {
                 alert(`${students[student].name} disapproves!`);
             }
-            student++;
-            score.value = '';
-            studentName.innerHTML = `Student's name: <span>${students[student].name}</span>`;
+            student++; // Pass to the next student.
+            score.value = ''; // Reset the initial value of the input.
+            studentName.innerHTML = `Student's name: <span>${students[student].name}</span>`; // reset the name of the student displayed.
         } else {
             result.classList.add('red');
             result.textContent = 'You need to introduce a number!';
         }
 })
 
-// Validates 
+// Validates if the score entered was a number.
 const validateScore = () => {
     if (score.value/0 == Infinity) { // This cannot accept the score 0 because 0/0 != infinity (but also nobody gets a 0 in a test)
         return true;
@@ -40,6 +42,7 @@ const calculateAverage = (student) => {
     return((parseInt(score.value) + students[student]["scoreOne"]+ students[student]["scoreTwo"])/ 3); // Transform the score.value in an integer (like the professors do)
 }
 
+// Objets students contain the name and the score of the two past practical works.
 let students = [{
     name: 'Horacio Rodriguez',
     scoreOne: 6,
@@ -62,7 +65,7 @@ let students = [{
     scoreTwo: 6,
 }];
 
-// Using try & catch
+// Using try & catch for the first student displayed in DOM.
 try { studentName.innerHTML = `Student's name: <span>${students[0].name}</span>`; }
 catch (error) {
     console.log('Failed to load the student name, error info: ', error.info);
